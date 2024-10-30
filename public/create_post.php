0f22617e412f97a485200f55078980d8450bb3df
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-10-30 10:53:05 -0300

    feat: add composerjson

--- public/create_post.php
+++ public/create_post.php
@@ -6,10 +6,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Captura os dados do formulário
     $title = $_POST['title'];
     $content = $_POST['content'];
+    $author = $_POST['author'];
 
     // Prepara a inserção no banco de dados
-    $stmt = $pdo->prepare('INSERT INTO posts (title, content) VALUES (?, ?)');
-    $stmt->execute([$title, $content]);
+    $stmt = $pdo->prepare('INSERT INTO posts (title, content, author) VALUES (?, ?, ?)');
+    $stmt->execute([$title, $content, $author]);
 
     echo "<p>Post criado com sucesso!</p>";
 }
@@ -19,6 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <form action="" method="POST">
     <label for="title">Título:</label><br>
     <input type="text" id="title" name="title" required><br><br>
+
+    <label for="author">Autor:</label><br>
+    <input type="text" id="author" name="author" required><br><br>
     
     <label for="content">Conteúdo:</label><br>
     <textarea id="content" name="content" rows="4" required></textarea><br><br>
--- public/index.php
+++ public/index.php
@@ -5,13 +5,56 @@ include '../templates/header.php';
 $stmt = $pdo->query('SELECT * FROM posts ORDER BY created_at DESC');
 $posts = $stmt->fetchAll();
 
-?>
+function formataMes($mes){
+    if($mes === 1) {
+        echo "Janeiro";
+    } elseif($mes === 2) {
+        echo "Fevereiro";
+    } elseif($mes === 3) {
+        echo "Março";
+    } elseif($mes === 4) {
+        echo "Abril";
+    } elseif($mes === 5) {
+        echo "Maio";
+    } elseif($mes === 6) {
+        echo "Junho";
+    } elseif($mes === 7) {
+        echo "Julho";
+    } elseif($mes === 8) {
+        echo "Agosto";
+    } elseif($mes === 9) {
+        echo "Setembro";
+    } elseif($mes === 10) {
+        echo "Outubro";
+    } elseif($mes === 11) {
+        echo "Novembro";
+    } elseif($mes === 12) {
+        echo "Dezembro";
+    } else {
+        echo "Mês inválido";
+    }
+}
 
-<h1>Blog</h1>
+function formataData($data){
+    $dia = (int)($data[8] . $data[9]);
+    $mes = (int)($data[5] . $data[6]);
+    $ano = (int)($data[0] . $data[1] . $data[2] . $data[3]);
+
+    echo "$dia de ";
+    echo formataMes($mes);
+    echo " de $ano";
+}
+function formataHora($hora){
+    echo "$hora[11]$hora[12]$hora[13]$hora[14]$hora[15]";
+}
+//<?= formataData(htmlspecialchars($post['created_at'])) ?>
+
+
+<h1>Webinfo</h1>
 <a href="create_post.php">Criar Novo Post</a>
 <?php foreach ($posts as $post): ?>
     <h2><a href="post.php?id=<?= $post['id'] ?>"><?= htmlspecialchars($post['title']) ?></a></h2>
-    <p><?= htmlspecialchars($post['content']) ?></p>
+    <p>Postado por <?= htmlspecialchars($post['author']) ?> às <?= formataHora(htmlspecialchars($post['created_at'])) ?> em <?= formataData(htmlspecialchars($post['created_at'])) ?></p>
     <hr>
 <?php endforeach; ?>
 
