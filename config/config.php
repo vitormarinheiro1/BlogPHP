85bd64f05fbbd32b786bc5a6169ebf84333f2616
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-10-30 11:43:33 -0300

    Configurar conexão com JawsDB

--- config/config.php
+++ config/config.php
@@ -1,8 +1,12 @@
 <?php
-$host = 'localhost';
-$db = 'blog_db';
-$user = 'root';
-$pass = '';
+// Obtendo a URL de conexão do JawsDB
+$dbUrl = parse_url(getenv("JAWSDB_URL"));
+
+// Extraindo as informações de conexão
+$host = $dbUrl["host"];
+$db = ltrim($dbUrl["path"], '/'); // Remove a barra inicial
+$user = $dbUrl["user"];
+$pass = $dbUrl["pass"];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
